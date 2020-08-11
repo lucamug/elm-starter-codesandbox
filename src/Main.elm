1a5825b15488d6c1675f7b0c611b61567b94d6e3
Luca <lucamug@users.noreply.github.com>
2020-08-12 06:57:41 +0900

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,10 @@
+module Main exposing (main)
+
+import Html exposing (text, div)
+import Html.Attributes exposing (style)
+
+main = div [style "font-size" "96px"] [text "Elm seems to be working!"]
+
+
+-- to add packages just type "npx elm install name-of/package"
+-- in a new terminal (the "+"" to the right of "yarn start" )
